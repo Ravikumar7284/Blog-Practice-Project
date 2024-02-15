40917e7016978b2f4bf7f420ed9ca51fe3c0f14b
Ravikumar7284 <ravikumarrajhans11@outlook.com>
2024-02-15 13:16:52 +0530

    Post entity created and relations mapped with User and Category

---
+++ src/main/java/com/blog/BlogWeb/entity/Post.java
@@ -0,0 +1,44 @@
+package com.blog.BlogWeb.entity;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "post")
+public class Post {
+
+  @Id
+  @Column(name = "post_id", unique = true, nullable = false)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(name = "post_title", nullable = false, length = 100)
+  private String title;
+
+  @Column(name = "post_content", length = 1000)
+  private String content;
+
+  @Column(name = "post_image_name")
+  private String imageName;
+
+  @Column(name = "post_added_date")
+  private Date addedDate;
+
+  @ManyToOne
+  @JoinColumn(name = "category_id")
+  private Category category;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+}
--- src/main/java/com/blog/BlogWeb/entity/User.java
+++ src/main/java/com/blog/BlogWeb/entity/User.java
@@ -1,11 +1,16 @@
 package com.blog.BlogWeb.entity;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Data;
 
@@ -31,4 +36,7 @@ public class User {
 
   @Column(name = "user_about", nullable = false)
   private String about;
+
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+  private List<Post> posts = new ArrayList<>();
 }
